5086fbafc48f89e1b0d804bf463981d91a673bdb
dan rassi <dan.rassi@gmail.com>
2010-03-11 21:42:56 -0500

    add filesize, mod date, id3

--- controllers/hello.py
+++ controllers/hello.py
@@ -3,10 +3,9 @@ import time
 
 import logging
 
-import couchdb
-
 from couchdb.client import Server, Database
-from couchdb.schema import Document, TextField
+
+from mutagen.easyid3 import EasyID3
 
 from pylons import request, response, session, tmpl_context as c
 from pylons.controllers.util import abort, redirect_to
@@ -15,9 +14,11 @@ from scatterbrainz.lib.base import BaseController, render
 
 log = logging.getLogger(__name__)
 
-class Track(Document):
-    dirname = TextField()
-    filename = TextField()
+def getFullPath(track):
+    return '/' + '/'.join(track['filepath']) + '/' + track['filename']
+
+def utf8(s):
+    return s.decode('utf-8')
 
 class HelloController(BaseController):
 
@@ -34,7 +35,10 @@ class HelloController(BaseController):
             del server['scatterbrainz']
             db = server.create('scatterbrainz')
             log.info('database deleted and created')
+        
+        now = time.time()
 
+        id = 0
         numFiles = 0
         numLoaded = 0
         numInserts = 0
@@ -43,21 +47,44 @@ class HelloController(BaseController):
         for dirname, dirnames, filenames in os.walk('/media/data/music/Bob Dylan'):
             for filename in filenames:
                 numFiles = numFiles + 1
-                id = str(numLoaded).rjust(10,'0')
-                reldir = os.path.relpath(dirname, '/media/data/music')
+                idStr = str(id).rjust(10,'0')
+                
+                # get path, size, date
+                reldir = os.path.relpath(dirname, '/media/data/music').strip('/').split('/')
+                filepath = os.path.join(dirname,filename)
+                size = os.path.getsize(filepath)
+                mtime = os.path.getmtime(filepath)
+                
+                # id3
+                id3dumb = EasyID3(filepath)
+                id3 = {}
+                for key in id3dumb:
+                    id3[key] = id3dumb[key][0]
+                
                 try:
-                    tracks.append(Track(id=id, dirname=reldir, filename=filename))
-                    numLoaded = numLoaded + 1
+                    track = {
+                             '_id'      : idStr,
+                             'doctype'  : 'Track',
+                             'filepath' : map(utf8, reldir),
+                             'filename' : utf8(filename),
+                             'size'     : size,
+                             'added'    : round(now),
+                             'mtime'    : round(mtime),
+                             'id3'      : id3
+                            }
+                    tracks.append(track)
+                    id = id + 1
                 except UnicodeDecodeError:
-                    log.info('UnicodeDecodeError  '+reldir+' '+filename)
                     numBad = numBad + 1
-                if numLoaded % 1000 == 0:
+                if len(tracks) == 1000:
                     db.update(tracks)
                     numInserts = numInserts + 1
+                    numLoaded = numLoaded + len(tracks)
                     tracks = []
         if tracks:
             db.update(tracks)
             numInserts = numInserts + 1
+            numLoaded = numLoaded + len(tracks)
 
         return """Saw %(numFiles)d files, loaded %(numLoaded)d in %(numInserts)d inserts, %(numBad)d failed
               due to encoding problems.""" \
@@ -65,10 +92,11 @@ class HelloController(BaseController):
                   'numBad':numBad}
 
     def index(self):
-        db = couchdb.client.Database('http://localhost:5984/scatterbrainz')
+        db = Database('http://localhost:5984/scatterbrainz')
         tracks = []
-        for id in db:
-            tracks.append(Track.load(db, id))
+        for key in db:
+            tracks.append(db[key])
         c.tracks = tracks
+        c.getFullPath = getFullPath
         return render('/hello.html')
 
